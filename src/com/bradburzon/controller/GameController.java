87d2002c50afc71e77dfc83e263a0f9f3980a584
Brad Burzon <bradburzon@ymail.com>
2019-02-01 16:02:37 -0500

    added functions to be implemented

--- src/com/bradburzon/controller/GameController.java
+++ src/com/bradburzon/controller/GameController.java
@@ -12,18 +12,56 @@ GameController
 * As the game, I should be able to make a new game.
 * As the game, I should be able to reset the game.
 * As the game, I should be able to make a move.
-
  */
 public class GameController {
 	
-	GameModel gameModel;
-	Player currentPlayer;
+	private GameModel gameModel;
+	private Player currentPlayer;
+	private Player nextPlayer;
 	
 	
 	public GameController(GameModel gameModel) {
-		this.gameModel = gameModel;
+		this.gameModel = gameModel; //new GameModel(Player, Player)
+		//initiate() need to ask what letter player 1 is
+		currentPlayer = gameModel.getPlayer1();
+		nextPlayer = gameModel.getPlayer2();
 	}
 	
+	/**
+	 * Resets the game including the board, score, and previous moves. 
+	 */
+	public void resetGame() {
+		
+	}
+	
+	/**
+	 * Creates a new game while keeping the score....
+	 */
+	public void newGame() {
+		
+	}
+	
+	/**
+	 * Lets the currentPlayer to make a move on the board
+	 */
+	public void move() {
+		
+	}
+	
+	/**
+	 * Swap the currentPlayer and nextPlayer
+	 */
+	public void switchPlayer() {
+		
+	}
+	
+	/**
+	 * Checks if there is a winner or not
+	 * @return The status of the board if one of the player has won or there is no winner
+	 */
+	public boolean checkBoardStatus(){
+		return false;
+	}
 	/*
 	public void initiate() {
 		
