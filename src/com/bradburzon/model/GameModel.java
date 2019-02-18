20a4ba2d2ae4b7f856746ec796987b0079488623
bradburzon <bradburzon@ymail.com>
2019-02-18 16:08:34 -0500

    took some variable members and put into player class

--- src/com/bradburzon/model/GameModel.java
+++ src/com/bradburzon/model/GameModel.java
@@ -3,31 +3,32 @@ package com.bradburzon.model;
 import java.util.HashMap;
 
 /*
-* As the game, I should be able to track the num of moves.
-* As the game, I should be able to keep score.
-* As the game, I should be able to keep track of the players.
-** As the game, I should be able to keep track of the board.
-** As the game, I should be able to know the rules and help.
+ * As the game, I should be able to track the num of moves.
+ * As the game, I should be able to keep score.
+ * As the game, I should be able to keep track of the players.
+ ** As the game, I should be able to keep track of the board.
+ ** As the game, I should be able to know the rules and help.
  */
 public class GameModel {
-	
+
 	private final Player player1;
 	private final Player player2;
-	private int[] player1Moves;
-	private int[] player2Moves;
 	private HashMap<Player, Integer> scores;
-	private int[][] board; //need a board
+	private char[][] board; //need a board
 	private final String rulesAndHelp;
-	
+
 	public GameModel(Player player1, Player player2) {
-		this.player1 = player1;
+		this.player1 = player1;  //seems like it will be better if I make a class Player with enum X and O member
 		this.player2 = player2;
-		player1Moves = new int[5];
-		player2Moves = new int[5];
 		scores = new HashMap<Player, Integer>();
 		scores.put(player1, 0);
 		scores.put(player2, 0);
 		rulesAndHelp = "";
+		board = new char[3][3];
+	}
+
+	public GameModel() {
+		this(new Player(Letter.X), new Player(Letter.O));
 	}
 
 	/**
@@ -45,20 +46,6 @@ public class GameModel {
 	}
 
 	/**
-	 * @return the player1Moves
-	 */
-	public int[] getPlayer1Moves() {
-		return player1Moves;
-	}
-
-	/**
-	 * @return the player2Moves
-	 */
-	public int[] getPlayer2Moves() {
-		return player2Moves;
-	}
-
-	/**
 	 * @return the scores
 	 */
 	public HashMap<Player, Integer> getScores() {
@@ -68,8 +55,15 @@ public class GameModel {
 	/**
 	 * @return the board
 	 */
-	public int[][] getBoard() {
+	public char[][] getBoard() {
 		return board;
+	}
+
+	/**
+	 * @param board the board to set
+	 */
+	public void setBoard(char[][] board) {
+		this.board = board;
 	}
 
 	/**
